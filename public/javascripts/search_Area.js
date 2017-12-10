7437c395423ce7044ae018a001d7e45430d2e632
Samuel Ethier <ethiersamuel@hotmail.com>
2017-12-10 14:55:59 -0500

    raml complete

--- public/javascripts/search_Area.js
+++ public/javascripts/search_Area.js
@@ -10,23 +10,8 @@ $(document).ready(function () {
             if (status == "success") {
                 //To clear the table to be ready for another search
                 $("tbody").empty();
-                var content = "<tr><th>ID</th><th>Type</th><th>Nom</th><th>Arrondissement</th><th>Condition</th></tr>";
-                var installations = data;
-                var area_Installation;
-                var name_Installation;
-                var id_Installation;
-                var type_Installation;
-                var condition_Installation;
-                for (var installation in installations) {
-                    area_Installation = installations[installation].area;
-                    name_Installation = installations[installation].name;
-                    id_Installation = installations[installation]._id;
-                    type_Installation = installations[installation].type;
-                    condition_Installation = installations[installation].condition;
-                    content += "<tr><td>" + id_Installation + "</td><td>" + type_Installation + "</td><td>"
-                     + name_Installation + "</td><td>" + area_Installation + "</td><td>" + condition_Installation + "</td></tr>";
-                }
-                $("tbody").append(content);
+                area_Html(data);
+                
             }else{
                 $("body").empty();
                 $("#table").append("<h1>Une erreur est survenu, nos développeurs tente présentement de régler le problème. Veuillez patienter.</h1>");
@@ -36,3 +21,23 @@ $(document).ready(function () {
     });
 
 });
+
+function area_Html(data){
+    var content = "<tr><th>ID</th><th>Type</th><th>Nom</th><th>Arrondissement</th><th>Condition</th></tr>";
+    var installations = data;
+    var area_Installation;
+    var name_Installation;
+    var id_Installation;
+    var type_Installation;
+    var condition_Installation;
+    for (var installation in installations) {
+        area_Installation = installations[installation].area;
+        name_Installation = installations[installation].name;
+        id_Installation = installations[installation]._id;
+        type_Installation = installations[installation].type;
+        condition_Installation = installations[installation].condition;
+        content += "<tr><td>" + id_Installation + "</td><td>" + type_Installation + "</td><td>"
+         + name_Installation + "</td><td>" + area_Installation + "</td><td>" + condition_Installation + "</td></tr>";
+    }
+    $("tbody").append(content);
+}
